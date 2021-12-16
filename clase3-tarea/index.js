1409fafb1b6e2f3d3a6e4ba04b7043f5a626460e
Jesus Alejandro <jesusalejandrosalas@hotmail.com>
2021-12-15 21:24:35 -0500

    tarea3

---
+++ clase3-tarea/index.js
@@ -0,0 +1,10 @@
+let medicinas = ``;
+let medicina = prompt(`Ingresa Una a una las medicinas que quieres. \n Si No necesitas mas medicinas, puedes escribir la palabra termina`);
+while ( medicina !== 'termina' ) {
+     medicinas += medicina + '\n'
+     medicina = prompt(`Ingresa Una a una las medicinas que quieres. \n Si No necesitas mas medicinas, puedes escribir la palabra termina`);
+   
+
+};
+
+alert(`Tu factura contiene los siguientes medicamentos: \n ${ medicinas }`);
