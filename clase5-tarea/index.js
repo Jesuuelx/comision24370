16507bc3b98a6133b457b4e1370fc2033cd442ee
Jesus Alejandro <jesusalejandrosalas@hotmail.com>
2021-12-28 15:28:22 -0500

    clase5

---
+++ clase5-tarea/index.js
@@ -0,0 +1,44 @@
+class Mascoteria  {
+
+    constructor ( nombreCliente, producto, medioPago, saldo ) {
+
+        this.nombreCliente = nombreCliente;
+        this.producto      = producto;
+        this.medioPago     = medioPago;
+        this.saldo         = saldo;
+
+
+
+
+    }  
+
+
+    recargaTuSaldo ( recarga ) {
+        this.saldo = this.saldo + recarga;
+    }
+
+
+    pagoProducto ( valorProducto ) {
+        if (( this.saldo > 0 ) && (valorProducto < this.saldo )) {
+
+            this.saldo = this.saldo - valorProducto;
+
+
+        }
+
+
+
+    }
+
+    
+
+
+
+
+}
+
+const mascotaUno = new Mascoteria('Moscu', 'Shampoo', 'efectivo', 1000);
+mascotaUno.pagoProducto(500);
+mascotaUno.recargaTuSaldo(400);
+
+console.log( mascotaUno );
